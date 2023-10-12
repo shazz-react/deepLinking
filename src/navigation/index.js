e35bf30fe684c2fb79b1ac975a5ac209423a14dd
Shahabaz Khan <shahbaz.9676@gmail.com>
2023-10-13 00:53:36 +0530

    Commit: Event listeners for links

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ActivityIndicator} from 'react-native';
+import {ActivityIndicator, Linking} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Home from '../screens/Home';
@@ -7,6 +7,22 @@ import Details from '../screens/Details';
 
 const linking = {
   prefixes: ['shahabazsapp://'],
+  async getInitialURL() {
+    const url = await Linking.getInitialURL();
+    return url;
+  },
+
+  // Custom function to subscribe to incoming links
+  subscribe(listener) {
+    // Listen to incoming links from deep linking
+    const linkingSubscription = Linking.addEventListener('url', ({url}) => {
+      listener(url);
+    });
+    return () => {
+      // Clean up the event listeners
+      linkingSubscription.remove();
+    };
+  },
   config: {
     initialRouteName: 'Home',
     screens: {
